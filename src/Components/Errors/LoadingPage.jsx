f0bd16709eb6c80fe6e36774cf68c47baa7c070c
Ariful Islam <arifulq234@gmail.com>
2025-11-27 13:54:37 +0600

    soling

--- src/Components/Errors/LoadingPage.jsx
+++ src/Components/Errors/LoadingPage.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+// import React from 'react';
 
-const LoadingPage = () => {
-    return (
-        <div className='min-h-screen flex items-center space-y-2 flex-col justify-center'>
-            <span className="loading loading-spinner loading-xl"></span>
-            <p className='text-gray-600 text-2xl'>Loading</p>
-        </div>
-    );
-};
+// const LoadingPage = () => {
+//     return (
+//         <div className='min-h-screen flex items-center space-y-2 flex-col justify-center'>
+//             <span className="loading loading-spinner loading-xl"></span>
+//             <p className='text-gray-600 text-2xl'>Loading</p>
+//         </div>
+//     );
+// };
 
-export default LoadingPage;
+// export default LoadingPage;
